e54930e6341af35cb803af0a3670b33f705b411e
Charles COLELLA <charlescol@hotmail.fr>
2023-11-24 13:58:42 -0500

    [ADD] factory & [CODE CLEANING]

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,39 @@
-import { ApiOperation, ApiResponse } from "@nestjs/swagger";
-import { Controller, Get, HttpCode, Query } from "@nestjs/common";
+import { ApiOperation, ApiQuery, ApiResponse } from "@nestjs/swagger";
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  Query,
+} from "@nestjs/common";
 import AppService from "./app.service";
 
 @Controller()
 export default class AppController {
   constructor(private readonly service: AppService) {}
 
-  @ApiOperation({ summary: "Create a 'Hello World' greeting message." })
-  @ApiResponse({
-    status: 200,
-    description: "Successfully generated 'Hello World' message",
+  @ApiOperation({
+    summary: "Create a 'Hello World' greeting message.",
+    description:
+      "Generates a greeting message. If a valid name is provided, it returns 'Hello World [name]!'. If the name is invalid, it throws a BadRequestException.",
   })
   @ApiResponse({
     status: 200,
-    description: "Did not validate the provided name",
+    description:
+      "Successfully generated 'Hello World' message with the provided valid name.",
+    type: String,
+  })
+  @ApiResponse({
+    status: 400,
+    description: "Bad Request: The provided name did not pass validation.",
+    type: BadRequestException,
+  })
+  @ApiQuery({
+    name: "name",
+    required: false,
+    description:
+      "The name to be included in the greeting message. Should be a valid string without special characters.",
+    type: String,
   })
   @HttpCode(200)
   @Get("hello")
