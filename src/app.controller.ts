79f9afa5369ea4ba1bc299b4f44ab68cffd98b90
charlescol <charlescol@hotmail.fr>
2025-03-20 00:05:49 +0100

    chore: upgrade dependencies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, HttpCode, Query } from "@nestjs/common";
-import AppService from "./app.service";
+import { Controller, Get, HttpCode, Query } from '@nestjs/common';
+import AppService from './app.service';
 
-@Controller({ version: "1" })
+@Controller({ version: '1' })
 export default class AppController {
   constructor(private readonly service: AppService) {}
 
   @HttpCode(200)
-  @Get("hello")
-  getHello(@Query("name") name?: string): string {
+  @Get('hello')
+  getHello(@Query('name') name?: string): string {
     return this.service.getHello(name);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import * as path from "path";
-import { Module } from "@nestjs/common/decorators";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import globalConfig from "@configs/global.config";
-import postgresConfig from "@configs/database/postgres.config";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import validationSchema from "@configs/joi/validationSchema";
-import AppController from "./app.controller";
-import AppService from "./app.service";
+import * as path from 'path';
+import { Module } from '@nestjs/common/decorators';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import globalConfig from '@configs/global.config';
+import postgresConfig from '@configs/database/postgres.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import validationSchema from '@configs/joi/validationSchema';
+import AppController from './app.controller';
+import AppService from './app.service';
 
 @Module({
   controllers: [AppController],
@@ -15,17 +15,13 @@ import AppService from "./app.service";
     ConfigModule.forRoot({
       isGlobal: true,
       load: [globalConfig, postgresConfig],
-      envFilePath: path.resolve(
-        process.cwd(),
-        "env",
-        `.env.${process.env.NODE_ENV || "local"}`
-      ),
+      envFilePath: path.resolve(process.cwd(), 'env', `.env.${process.env.NODE_ENV || 'local'}`),
       validationSchema,
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        ...configService.get("database"),
+        ...configService.get('database'),
       }),
     }),
   ],
